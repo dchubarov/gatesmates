c6a929a2b8fba2d955adba72984a4a229cad86eb
bubo <bubo@twowls.org>
2016-12-10 16:48:49 +0300

    Initial commit

---
+++ registry/src/main/java/org/twowls/gatesmates/registry/Registry.java
@@ -0,0 +1,9 @@
+package org.twowls.gatesmates.registry;
+
+/**
+ * <p>Provides utility methods to access Windows registry.</p>
+ *
+ * @author bubo &lt;bubo@twowls.org&gt;
+ */
+public class Registry {
+}
---
+++ util/src/main/java/org/twowls/gatesmates/util/Handle.java
@@ -0,0 +1,10 @@
+package org.twowls.gatesmates.util;
+
+/**
+ * <p>Represents operating system handle.</p>
+ *
+ * @author bubo &lt;bubo@twowls.org&gt;
+ */
+public interface Handle extends AutoCloseable {
+
+}
